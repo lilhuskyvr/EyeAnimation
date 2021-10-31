bda9b4726c4077c1fe3d63aed562d63098efc9a1
lilhuskyvr <lilhuskyvr@gmail.com>
2021-11-01 10:42:29 +1100

    upgraded to u10

--- EyeAnimation/EyeAnimation.cs
+++ EyeAnimation/EyeAnimation.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Linq;
+using System.Reflection;
+using RainyReignGames.MeshDismemberment;
 using ThunderRoad;
 using UnityEngine;
 
@@ -10,6 +12,7 @@ namespace EyeAnimation
         private Creature _creature;
         private float _closedUpperLid;
         private float _closedLowerLid;
+        private Dismemberment _dismemberment;
         public Transform leftUpperLid;
         public Transform rightUpperLid;
         public Transform leftLowerLid;
@@ -23,31 +26,51 @@ namespace EyeAnimation
 
             leftUpperLid =
                 _creature.manikinParts.Rig.bones.FirstOrDefault(boneCondition =>
-                    boneCondition.Value.name == "LeftUpperLid_Mesh").Value;
+                    boneCondition.Value.boneTransform.name == "LeftUpperLid_Mesh").Value.boneTransform;
             rightUpperLid =
                 _creature.manikinParts.Rig.bones.FirstOrDefault(boneCondition =>
-                    boneCondition.Value.name == "RightUpperLid_Mesh").Value;
+                    boneCondition.Value.boneTransform.name == "RightUpperLid_Mesh").Value.boneTransform;
             leftLowerLid =
                 _creature.manikinParts.Rig.bones.FirstOrDefault(boneCondition =>
-                    boneCondition.Value.name == "LeftLowerLid_Mesh").Value;
+                    boneCondition.Value.boneTransform.name == "LeftLowerLid_Mesh").Value.boneTransform;
             rightLowerLid =
                 _creature.manikinParts.Rig.bones.FirstOrDefault(boneCondition =>
-                    boneCondition.Value.name == "RightLowerLid_Mesh").Value;
+                    boneCondition.Value.boneTransform.name == "RightLowerLid_Mesh").Value.boneTransform;
             _creature.OnKillEvent += CreatureOnOnKillEvent;
-            // _creature.OnDamageEvent += CreatureOnOnDamageEvent;
-            // _creature.ragdoll.OnStateChange += RagdollOnOnStateChange;
+
+            var creatureRagdoll = _creature.ragdoll;
+
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                                     | BindingFlags.Static;
+            _dismemberment =
+                creatureRagdoll.GetType().GetField("dismemberment", bindFlags).GetValue(creatureRagdoll) as
+                    Dismemberment;
+
+            _dismemberment.Completed += DismembermentOnCompleted;
         }
 
-        private void RagdollOnOnStateChange(Ragdoll.State state)
+        private void DismembermentOnCompleted(object sender, Dismemberment.CompletedEventArgs e)
         {
-            Debug.Log("Is state" + (state == Ragdoll.State.Destabilized));
-        }
+            Debug.Log("completed");
+            var severedMesh = e.splitGameObject;
 
-        private void CreatureOnOnDamageEvent(CollisionInstance collisioninstance)
-        {
-            _creature.Kill();
-            if (_creature.state == Creature.State.Alive)
-                StartCoroutine(FlashingEyes());
+            foreach (var skinnedMeshRenderer in severedMesh.GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                if (skinnedMeshRenderer.name.Contains("Eyes_"))
+                {
+                    foreach (var bone in skinnedMeshRenderer.bones)
+                    {
+                        if (bone.name.Contains("UpperLid"))
+                        {
+                            StartCoroutine(AnimateLocalRotation(bone, 5, Quaternion.Euler(0, _closedUpperLid, 0)));
+                        }
+                        if (bone.name.Contains("LowerLid"))
+                        {
+                            StartCoroutine(AnimateLocalRotation(bone, 5, Quaternion.Euler(0, _closedLowerLid, 0)));
+                        }
+                    }
+                }
+            }
         }
 
         private void CreatureOnOnKillEvent(CollisionInstance collisioninstance, EventTime eventtime)
@@ -74,32 +97,6 @@ namespace EyeAnimation
             yield return null;
         }
 
-        private IEnumerator AnimateLocalRotationBackAndForth(Transform animatedTransform, float duration,
-            Quaternion targetLocalRotation)
-        {
-            var startLocalRotation = animatedTransform.localRotation;
-            var startTime = Time.time;
-
-            var halfDuration = duration / 2;
-            while (Time.time - startTime < halfDuration)
-            {
-                animatedTransform.localRotation = Quaternion.Lerp(startLocalRotation, targetLocalRotation,
-                    (Time.time - startTime) / halfDuration);
-                yield return new WaitForFixedUpdate();
-            }
-
-            startTime = Time.time;
-
-            while (Time.time - startTime < halfDuration)
-            {
-                animatedTransform.localRotation = Quaternion.Lerp(targetLocalRotation, startLocalRotation,
-                    (Time.time - startTime) / halfDuration);
-                yield return new WaitForFixedUpdate();
-            }
-
-            yield return null;
-        }
-
         public IEnumerator ClosingEyes(float duration = 2)
         {
             StartCoroutine(AnimateLocalRotation(leftUpperLid, duration, Quaternion.Euler(0, _closedUpperLid, 0)));
@@ -110,21 +107,10 @@ namespace EyeAnimation
             yield return null;
         }
 
-        public IEnumerator FlashingEyes(float duration = 0.5f)
-        {
-            StartCoroutine(AnimateLocalRotationBackAndForth(leftUpperLid, 5, Quaternion.Euler(0, 0, 0)));
-            StartCoroutine(AnimateLocalRotationBackAndForth(rightUpperLid, 5, Quaternion.Euler(0, 0, 0)));
-            StartCoroutine(AnimateLocalRotationBackAndForth(leftLowerLid, 5, Quaternion.Euler(0, 0, 0)));
-            StartCoroutine(AnimateLocalRotationBackAndForth(rightLowerLid, 5, Quaternion.Euler(0, 0, 0)));
-
-            yield return null;
-        }
-
         private void OnDestroy()
         {
             _creature.OnKillEvent -= CreatureOnOnKillEvent;
-            // _creature.OnDamageEvent -= CreatureOnOnDamageEvent;
-            // _creature.ragdoll.OnStateChange -= RagdollOnOnStateChange;
+            _dismemberment.Completed -= DismembermentOnCompleted;
         }
     }
 }
--- EyeAnimation/EyeAnimationLevelModule.cs
+++ EyeAnimation/EyeAnimationLevelModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using IngameDebugConsole;
 using ThunderRoad;
 using UnityEngine;
 
@@ -6,11 +7,37 @@ namespace EyeAnimation
 {
     public class EyeAnimationLevelModule : LevelModule
     {
-        public override IEnumerator OnLoadCoroutine(Level level)
+        public override IEnumerator OnLoadCoroutine()
         {
             EventManager.onCreatureSpawn += EventManagerOnonCreatureSpawn;
             Debug.Log("Eye Animation");
-            return base.OnLoadCoroutine(level);
+
+            // DebugLogConsole.AddCommandInstance("creature_spawn_and_behead",
+            //     "TestDismemberment", "TestDismemberment",
+            //     this);
+
+            return base.OnLoadCoroutine();
+        }
+
+        
+        public void TestDismemberment()
+        {
+            GameManager.local.StartCoroutine(Catalog.GetData<CreatureData>("HumanFemale").SpawnCoroutine(
+                Player.local.transform.position + Player.local.transform.forward,
+                Player.local.transform.rotation,
+                null,
+                rsCreature =>
+                {
+                    GameManager.local.StartCoroutine(BeheadCreature(rsCreature));
+                }
+            ));
+        }
+
+        private IEnumerator BeheadCreature(Creature rsCreature)
+        {
+            yield return new WaitForSeconds(2);
+            rsCreature.ragdoll.headPart.Slice();
+            rsCreature.Kill();
         }
 
         private void EventManagerOnonCreatureSpawn(Creature creature)
